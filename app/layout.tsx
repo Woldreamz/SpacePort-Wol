728347a645dd66bd094e348a40cb790a1d5af64f
Rainer Nsa <woldreamz@gmail.com>
2024-05-05 18:33:48 +0100

    City of God

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import StarsCanvas from "@/components/main/StarBackground";
 import Navbar from "@/components/main/NavBar";
 import Footer from "@/components/main/Footer";
 
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -19,6 +20,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <link rel="icon" type="image/png" href="https://i.postimg.cc/xjwDWjgB/woldreamz-inc-favicon-black.png" />
       <body
         className={`${inter.className} bg-[#030014] overflow-y-scroll overflow-x-hidden`}
       >
